a0774bb3dedf4b622fed257a1b88b988c82f38c7
Omi <thibautf69220@gmail.com>
2022-11-21 11:09:59 +0100

    chore: Format standardization for astar

--- src/main/java/com/hexa17/pldagile/astar/HeuristicForNodesWithXYCoordinates.java
+++ src/main/java/com/hexa17/pldagile/astar/HeuristicForNodesWithXYCoordinates.java
@@ -8,93 +8,95 @@ import org.slf4j.LoggerFactory;
 
 public class HeuristicForNodesWithXYCoordinates implements Function<NodeWithXYCoordinates, Double> {
 
-  private static final Logger LOG =
-      LoggerFactory.getLogger(HeuristicForNodesWithXYCoordinates.class);
+    private static final Logger LOG = LoggerFactory.getLogger(HeuristicForNodesWithXYCoordinates.class);
 
-  private final double maxSpeed;
-  private final NodeWithXYCoordinates target;
+    private final double maxSpeed;
+    private final NodeWithXYCoordinates target;
 
-  /**
-   * Constructs the heuristic function for the specified graph and target node.
-   *
-   * @param graph the graph
-   * @param target the target node
-   */
-  public HeuristicForNodesWithXYCoordinates(
-      ValueGraph<NodeWithXYCoordinates, Double> graph, NodeWithXYCoordinates target) {
-    // We need the maximum speed possible on any path in the graph for the heuristic function to
-    // calculate the cost for a euclidean distance
-    this.maxSpeed = calculateMaxSpeed(graph);
-    this.target = target;
-  }
-
-  /**
-   * Calculates the maximum speed possible on any path in the graph. The speed of a path is
-   * calculated as: euclidean distance between the path's nodes, divided by its cost.
-   *
-   * @param graph the graph
-   * @return the maximum speed
-   */
-  private static double calculateMaxSpeed(ValueGraph<NodeWithXYCoordinates, Double> graph) {
-    return graph.edges().stream()
-        .map(edge -> calculateSpeed(graph, edge))
-        .max(Double::compare)
-        .orElseThrow(() -> new IllegalArgumentException("Graph is empty"));
-  }
-
-  /**
-   * Calculates the speed on a path in the graph. The speed of a path is calculated as: euclidean
-   * distance between the path's nodes, divided by its cost.
-   *
-   * @param graph the graph
-   * @param edge the edge (= path)
-   * @return the speed
-   */
-  private static double calculateSpeed(
-      ValueGraph<NodeWithXYCoordinates, Double> graph, EndpointPair<NodeWithXYCoordinates> edge) {
-    double euclideanDistance = calculateEuclideanDistance(edge.nodeU(), edge.nodeV());
-    double cost =
-        graph.edgeValue(edge).orElseThrow(() -> new IllegalArgumentException("Graph is empty"));
-    double speed = euclideanDistance / cost;
-
-    if (LOG.isDebugEnabled()) {
-      LOG.debug(
-          "Calculated speed from {} to {}: euclideanDistance = {}, cost = {} --> speed = {}",
-          edge.nodeU(),
-          edge.nodeV(),
-          euclideanDistance,
-          cost,
-          speed);
+    /**
+     * Constructs the heuristic function for the specified graph and target node.
+     *
+     * @param graph  the graph
+     * @param target the target node
+     */
+    public HeuristicForNodesWithXYCoordinates(
+            ValueGraph<NodeWithXYCoordinates, Double> graph, NodeWithXYCoordinates target) {
+        // We need the maximum speed possible on any path in the graph for the heuristic
+        // function to
+        // calculate the cost for a euclidean distance
+        this.maxSpeed = calculateMaxSpeed(graph);
+        this.target = target;
     }
 
-    return speed;
-  }
+    /**
+     * Calculates the maximum speed possible on any path in the graph. The speed of
+     * a path is
+     * calculated as: euclidean distance between the path's nodes, divided by its
+     * cost.
+     *
+     * @param graph the graph
+     * @return the maximum speed
+     */
+    private static double calculateMaxSpeed(ValueGraph<NodeWithXYCoordinates, Double> graph) {
+        return graph.edges().stream()
+                .map(edge -> calculateSpeed(graph, edge))
+                .max(Double::compare)
+                .orElseThrow(() -> new IllegalArgumentException("Graph is empty"));
+    }
 
-  public static double calculateEuclideanDistance(
-      NodeWithXYCoordinates source, NodeWithXYCoordinates target) {
-    double distanceX = target.getX() - source.getX();
-    double distanceY = target.getY() - source.getY();
-    return Math.sqrt(distanceX * distanceX + distanceY * distanceY);
-  }
+    /**
+     * Calculates the speed on a path in the graph. The speed of a path is
+     * calculated as: euclidean
+     * distance between the path's nodes, divided by its cost.
+     *
+     * @param graph the graph
+     * @param edge  the edge (= path)
+     * @return the speed
+     */
+    private static double calculateSpeed(
+            ValueGraph<NodeWithXYCoordinates, Double> graph, EndpointPair<NodeWithXYCoordinates> edge) {
+        double euclideanDistance = calculateEuclideanDistance(edge.nodeU(), edge.nodeV());
+        double cost = graph.edgeValue(edge).orElseThrow(() -> new IllegalArgumentException("Graph is empty"));
+        double speed = euclideanDistance / cost;
 
-  /**
-   * Applies the heuristic function to the specified node.
-   *
-   * @param node the node
-   * @return the minimum cost for traveling from the specified node to the target
-   */
-  @Override
-  public Double apply(NodeWithXYCoordinates node) {
-    double euclideanDistance = calculateEuclideanDistance(node, target);
-    double minimumCost = euclideanDistance / maxSpeed;
+        if (LOG.isDebugEnabled()) {
+            LOG.debug(
+                    "Calculated speed from {} to {}: euclideanDistance = {}, cost = {} --> speed = {}",
+                    edge.nodeU(),
+                    edge.nodeV(),
+                    euclideanDistance,
+                    cost,
+                    speed);
+        }
 
-    LOG.debug(
-        "Applied heuristic to node {}: euclideanDistance = {}, maxSpeed = {} --> minimumCost = {}",
-        node,
-        euclideanDistance,
-        maxSpeed,
-        minimumCost);
+        return speed;
+    }
 
-    return minimumCost;
-  }
+    public static double calculateEuclideanDistance(
+            NodeWithXYCoordinates source, NodeWithXYCoordinates target) {
+        double distanceX = target.getX() - source.getX();
+        double distanceY = target.getY() - source.getY();
+        return Math.sqrt(distanceX * distanceX + distanceY * distanceY);
+    }
+
+    /**
+     * Applies the heuristic function to the specified node.
+     *
+     * @param node the node
+     * @return the minimum cost for traveling from the specified node to the target
+     */
+    @Override
+    public Double apply(NodeWithXYCoordinates node) {
+        double euclideanDistance = calculateEuclideanDistance(node, target);
+        double minimumCost = euclideanDistance / maxSpeed;
+
+        LOG.debug(
+                "Applied heuristic to node {}: euclideanDistance = {}, maxSpeed = {} --> minimumCost = {}",
+                node,
+                euclideanDistance,
+                maxSpeed,
+                minimumCost);
+
+        return minimumCost;
+    }
 }
--- src/main/java/com/hexa17/pldagile/astar/NodeWithXYCoordinates.java
+++ src/main/java/com/hexa17/pldagile/astar/NodeWithXYCoordinates.java
@@ -3,60 +3,62 @@ package com.hexa17.pldagile.astar;
 import java.util.Objects;
 
 /**
- * Data structure for graph nodes containing a name and X/Y coordinates. The name is the "primary
- * key" and used in the equals(), hashCode() and compareTo() method as unique ID.
+ * Data structure for graph nodes containing a name and X/Y coordinates. The
+ * name is the "primary
+ * key" and used in the equals(), hashCode() and compareTo() method as unique
+ * ID.
  *
  * @author <a href="sven@happycoders.eu">Sven Woltmann</a>
  */
 public class NodeWithXYCoordinates implements Comparable<NodeWithXYCoordinates> {
-  private final String name;
-  private final double x;
-  private final double y;
+    private final String name;
+    private final double x;
+    private final double y;
 
-  public NodeWithXYCoordinates(String name, double x, double y) {
-    this.name = name;
-    this.x = x;
-    this.y = y;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public double getX() {
-    return x;
-  }
-
-  public double getY() {
-    return y;
-  }
-
-  @Override
-  public boolean equals(Object other) {
-    if (this == other) {
-      return true;
-    }
-    if (other == null || getClass() != other.getClass()) {
-      return false;
+    public NodeWithXYCoordinates(String name, double x, double y) {
+        this.name = name;
+        this.x = x;
+        this.y = y;
     }
 
-    NodeWithXYCoordinates aStarNode = (NodeWithXYCoordinates) other;
+    public String getName() {
+        return name;
+    }
 
-    return Objects.equals(name, aStarNode.name);
-  }
+    public double getX() {
+        return x;
+    }
 
-  @Override
-  public int hashCode() {
-    return name != null ? name.hashCode() : 0;
-  }
+    public double getY() {
+        return y;
+    }
 
-  @Override
-  public int compareTo(NodeWithXYCoordinates other) {
-    return name.compareTo(other.name);
-  }
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (other == null || getClass() != other.getClass()) {
+            return false;
+        }
 
-  @Override
-  public String toString() {
-    return name;
-  }
+        NodeWithXYCoordinates aStarNode = (NodeWithXYCoordinates) other;
+
+        return Objects.equals(name, aStarNode.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
+
+    @Override
+    public int compareTo(NodeWithXYCoordinates other) {
+        return name.compareTo(other.name);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
