5d25421aa2fc355c8c76e7629650a381520059b7
Omi <thibautf69220@gmail.com>
2022-11-21 08:54:01 +0100

    chore: Create Maven project

---
+++ src/main/java/com/hexa17/pldagile/App.java
@@ -0,0 +1,13 @@
+package com.hexa17.pldagile;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
