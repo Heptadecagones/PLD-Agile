09dac9f64399f01e495b16752b3039089a52eac6
Henri <henri.bailleux@insa-lyon.fr>
2022-12-05 17:28:22 +0100

    wip(AjoutTourneeLivraison): toujours le robot

--- src/test/java/VueTest/IHMTest.java
+++ src/test/java/VueTest/IHMTest.java
@@ -7,7 +7,8 @@ import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
 import java.io.*;
-
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import controller.Controleur;
  
 public class IHMTest
@@ -16,23 +17,28 @@ public class IHMTest
                            AWTException, InterruptedException
     {
         Controleur c=new Controleur();
-        try {
-            Thread.sleep(2000);
-        }
-        catch (InterruptedException e)
-        {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        Runtime.getRuntime().exec("WordPad");
+
+        
         int keyInput[] = { KeyEvent.VK_J, KeyEvent.VK_A, KeyEvent.VK_V,
             KeyEvent.VK_A, KeyEvent.VK_SPACE };
+            
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        
+        /*Robot robot = new Robot();
+        robot.mouseMove(screenSize.width*1/3, 0);
+        robot.mousePress(0);
+        robot.delay(100);
+        robot.mouseRelease(0);
+        robot.delay(1000);*/
         c.view.obtenirBarre().obtenirCharger().doClick();
-        Robot robot = new Robot();
-        for (int i = 0; i < keyInput.length; i++) {
-          robot.keyPress(keyInput[i]);
-          robot.delay(100);
+        Thread.sleep(1000);
+        Robot robot2 = new Robot();
+        robot2.delay(2000);
+        robot2.delay(1000);
+        for(int i=0;i<keyInput.length;i++){
+            robot2.keyPress(keyInput[i]);
+            robot2.delay(100);
+            robot2.keyRelease(keyInput[i]);
         }
-
     }
-}
+}                                                                          
