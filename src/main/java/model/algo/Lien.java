b17f183f01682410ae9e6e759392b040a4464318
Omi <thibautf69220@gmail.com>
2022-12-08 15:47:41 +0100

    fixup! wip: more refactoring (Dijkstra - Solveur, moved algo)

--- src/main/java/model/algo/Lien.java
+++ src/main/java/model/algo/Lien.java
@@ -1,4 +1,4 @@
-package model.algo.Dijkstra;
+package model.algo;
 
 import java.util.ArrayList;
 
--- src/main/java/model/algo/Solveur.java
+++ src/main/java/model/algo/Solveur.java
@@ -1,4 +1,4 @@
-package model.algo.Dijkstra;
+package model.algo;
 
 import java.util.List;
 import java.util.ArrayList;
